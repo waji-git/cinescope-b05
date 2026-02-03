bc4ed2a16737220af467eba980ea746480b2f8a0
waji-git <wajifantasy@gmail.com>
2026-02-03 15:47:53 +0530

    hero banner

--- components/landing/hero-banner.tsx
+++ components/landing/hero-banner.tsx
@@ -6,7 +6,7 @@ export default function HeroBanner() {
     <section
       id="heroBanner"
       className="min-h-[100vh] w-full text-center py-16 bg-cover bg-center"
-      style={{ backgroundImage: "url('Hero.png')" }}
+      style={{ backgroundImage: "url('/Hero.png')" }}
     >
       heroBanner
     </section>
