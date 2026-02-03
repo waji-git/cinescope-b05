d89674f77ef35f27fbe6e503277a2fdc26e56876
waji-git <wajifantasy@gmail.com>
2026-02-03 16:53:35 +0530

    new ima

--- components/landing/hero-banner.tsx
+++ components/landing/hero-banner.tsx
@@ -1,32 +1,32 @@
 
 import React from "react";
 
-// export default function HeroBanner() {
-//   return (
-//     <section
-//       id="heroBanner"
-//       className="min-h-[100vh] w-full text-center py-16 bg-cover bg-center"
-//       style={{ backgroundImage: "url('/Hero.png')" }}
-    
-//     >
-//       heroBanner
-//     </section>
-//   );
-// }
-import Image from "next/image";
 export default function HeroBanner() {
   return (
-    <section className="relative min-h-screen w-full">
-      <Image
-        src="/Hero.png"
-        alt="Hero"
-        fill
-        priority
-        className="object-cover"
-      />
-      <div className="relative z-10 text-white text-center py-16">
-        heroBanner
-      </div>
+    <section
+      id="heroBanner"
+      className="min-h-[100vh] w-full text-center py-16 bg-cover bg-center"
+      style={{ backgroundImage: "url('/cine.png')" }}
+    
+    >
+      heroBanner
     </section>
   );
 }
+// import Image from "next/image";
+// export default function HeroBanner() {
+//   return (
+//     <section className="relative min-h-screen w-full">
+//       <Image
+//         src="/cine.png"
+//         alt="Hero"
+//         fill
+//         priority
+//         className="object-cover"
+//       />
+//       <div className="relative z-10 text-white text-center py-16">
+//         heroBanner
+//       </div>
+//     </section>
+//   );
+// }
